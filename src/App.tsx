0b0be40f2da953270fbbd8dbdc542b51c6ee12d4
IsraelCost <cmteisrael@gmail.com>
2022-09-16 15:26:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Header from 'header'
+
+const App = () => (
+  <Header title="Israel" />
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
